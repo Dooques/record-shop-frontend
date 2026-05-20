51a7f222d95b5b26f7485fd550352b15699b8b99
Dooques <samuelgdukes@gmail.com>
2026-05-20 09:07:31 +0100

    Init commit, create frotnend applciation which connects to the backend API

---
+++ RecordShopFrontend/RecordShopFrontend/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿namespace RecordShopFrontend.Models;
+
+public class Album(int id, string title, string artist, int releaseYear, string genre, int stock, string coverUrl)
+{
+    public int Id { get; set; } = id;
+    public string Title { get; set; } = title;
+    public string Artist { get; set; } = artist;
+    public int ReleaseYear { get; set; } = releaseYear;
+    public string Genre { get; set; } = genre;
+    public int Stock { get; set; } = stock;
+    public string CoverUrl { get; set; } = coverUrl;
+}
+
+public class AlbumDTO(int id, string? title, string? artist, int? releaseYear, string? genre, int stock, string coverUrl)
+{
+    public int Id { get; set; } = id;
+    public string? Title { get; set; } = title;
+    public string? Artist { get; set; } = artist;
+    public int? ReleaseYear { get; set; } = releaseYear;
+    public string? Genre { get; set; } = genre;
+    public int? Stock { get; set; } = stock;
+    public string? CoverUrl { get; set; } = coverUrl;
+}
